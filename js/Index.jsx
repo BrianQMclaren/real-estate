2e5d5d262925dfe7665b419becbc198fad672461
Brian McLaren <brianqmclaren@gmail.com>
2017-12-13 19:54:38 -0500

    layout html on listing component

--- js/Index.jsx
+++ js/Index.jsx
@@ -1,14 +1,14 @@
 import React from 'react';
 import Header from './Header';
 import Filter from './Filter';
-import Listing from './Listing';
+import Listings from './Listing';
 
 const Index = () => (
   <div className="app">
     <Header />
     <section className="">
       <Filter />
-      <Listing />
+      <Listings />
     </section>
   </div>
 );
--- js/Listing.jsx
+++ js/Listing.jsx
@@ -1,5 +1,67 @@
 import React from 'react';
 
-const Listing = () => <div className="Listing">Listing</div>;
+const Listings = () => (
+  <div className="Listings">
+    <section className="listing-heading">
+      <input type="text" name="search" />
+    </section>
+    <section className="listing-sort">
+      <div>390 results found</div>
+      <div className="listing-sortOptions">
+        <select name="sortby" className="sortby">
+          <option value="price-asc">Highest Price</option>
+          <option value="price-asc">Lowest Price</option>
+        </select>
+        <div className="view">
+          <i className="fa fa-th-list" aria-hidden="true" />
+          <i className="fa fa-th" aria-hidden="true" />
+        </div>
+      </div>
+    </section>
+    <section className="listing-results">
+      <div className="listing">
+        <div className="listing-img">
+          <span className="address">Address</span>
+          <div className="details">
+            <div className="user-img">
+              <div className="user-details">
+                <span className="user-name">Nina Smith</span>
+                <span className="post-date">05/05/2017</span>
+                <div className="listing-details">
+                  <div className="floor-space">
+                    <i className="fa fa-square-o" aria-hidden="true" />
+                    <span>1000 ft&sup2;</span>
+                  </div>
+                  <div className="bedrooms">
+                    <i className="fa fa-bed" aria-hidden="true" />
+                    <span>3 bedrooms</span>
+                  </div>
+                </div>
+              </div>
+              <div className="listing-rent">
+                <span>$2000 / month</span>
+                <span>
+                  <i className="fa fa-map-marker" aria-hidden="true" />Harlem,
+                  NY
+                </span>
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+    </section>
+    <section className="listing-pagination">
+      <ul className="pagination-num">
+        <li>Prev</li>
+        <li>1</li>
+        <li>2</li>
+        <li>3</li>
+        <li>4</li>
+        <li>5</li>
+        <li>Next</li>
+      </ul>
+    </section>
+  </div>
+);
 
-export default Listing;
+export default Listings;
